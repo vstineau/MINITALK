43289c66e332969939f23b7a077624393499174f
Vincent Stineau <vstineau@bess-f4r4s8.clusters.42paris.fr>
2024-02-22 17:20:13 +0100

    save base

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: vstineau <vstineau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:40:44 by vstineau          #+#    #+#             */
-/*   Updated: 2024/02/22 15:54:44 by vstineau         ###   ########.fr       */
+/*   Updated: 2024/02/22 17:19:48 by vstineau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,14 @@ int	main(int argc, char *argv[])
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_sigaction = handler;
+	sa.sa_flags = SA_SIGINFO;
 	if (argc != 3)
 		return (1);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	{
+		ft_putstr_fd("Erreur lors de l'instalation du gestionnaire de signal", 2);
+		return (1);
+	}
+	kill(ft_atoi(argv[1]), SIGUSR1);
+	return (0);
 }
