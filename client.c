62746d3c5bfcb21d99d1ba45a8474133cf3eb137
Vincent Stineau <vstineau@bess-f4r4s10.clusters.42paris.fr>
2024-02-23 17:58:46 +0100

    ca avance doucement

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: vstineau <vstineau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:40:44 by vstineau          #+#    #+#             */
-/*   Updated: 2024/02/22 17:40:24 by vstineau         ###   ########.fr       */
+/*   Updated: 2024/02/23 15:33:39 by vstineau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include "LIBFT/libft.h"
 
 void	handler(int signum, siginfo_t *info, void *context)
 {
 	ft_printf("signal recu par le client");
+
 	exit(0);
 }
 
-int	*send_message(int pid, char *message)
+void	send_message(int server_pid, char c)
 {
+	int	i;
+	int	bit;
+
+	i = 8;
+	while (i-- > 0)
+	{
+		bit = (c >> i) & 1;
+		if (bit == 1)
+			kill(server_pid, SIGUSR1);
+		else
+			kill(server_pid, SIGUSR2);
+		pause();
+	}
 }
 
 int	main(int argc, char *argv[])
 {
 	struct sigaction sa;
+	int	i;
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_sigaction = handler;
 	sa.sa_flags = SA_SIGINFO;
 	if (argc != 3)
 		return (1);
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
 	{
 		ft_putstr_fd("Erreur lors de l'instalation du gestionnaire de signal", 2);
 		return (1);
 	}
-	kill(ft_atoi(argv[1]), SIGUSR1);
+	i = 0;
+	while (argv[2][i])
+		send_message(ft_atoi(argv[1]), argv[2][i++]);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: vstineau <vstineau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:40:17 by vstineau          #+#    #+#             */
-/*   Updated: 2024/02/22 16:16:35 by vstineau         ###   ########.fr       */
+/*   Updated: 2024/02/23 17:01:09 by vstineau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include "LIBFT/ft_printf.h"
 #include "LIBFT/libft.h"
+
+typedef struct s_bit
+{
+	int		bit_received;
+	char	char_received;
+	int		bit_index;
+}					t_bit;
+
+static	t_bit	g_bit;
 
 void	handler(int signum, siginfo_t *info, void *context)
 {
 	ft_printf("signal recu par le server");
 	exit(0);
+}
+
+void	get_signal(int signal)
+{
+	char	*string;
+
+	string = NULL;
+	if (signal == SIGUSR1)
+		g_bit.bit_received = 1;
+	else
+		g_bit.bit_received = 0;
+	if (g_bit.bit_index == 8)
+		g_bit.bit_index = 0;
+	g_bit.char_received = (g_bit.char_received << 1) | g_bit.bit_received;
+	g_bit.bit_index++;
+	if (g_bit.bit_index == 8)
+		string = init_string(string, g_bit.char_received);
 }
 
 int	main(void)
@@ -31,7 +56,8 @@ int	main(void)
 	sigemptyset(&sa.sa_mask);
 	sa.sa_sigaction = handler;
 	sa.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	ft_printf("%d\n", getpid());
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
 	{
 		ft_putstr_fd("Erreur lors de l'instalation du gestionnaire de signal", 2);
 		return (1);
