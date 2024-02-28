9b69887375b598cc776bd955f3940cf8cca673d6
Vincent Stineau <vstineau@bess-f2r2s8.clusters.42paris.fr>
2024-02-28 11:53:37 +0100

    fix le leak sur le dernier accuse de reception et sur le strlen du server qui prenait pas de chaine valide comme pas de \0

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: vstineau <vstineau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:40:44 by vstineau          #+#    #+#             */
-/*   Updated: 2024/02/27 12:08:39 by vstineau         ###   ########.fr       */
+/*   Updated: 2024/02/28 11:44:05 by vstineau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,11 @@ void	send_bit(int server_pid, char c)
 {
 	int	bit;
 
-		bit = (c >> (g_send.bit_index % 8)) & 1;
-		if (bit == 1)
-			kill(server_pid, SIGUSR1);
-		else
-			kill(server_pid, SIGUSR2);
+	bit = (c >> (g_send.bit_index % 8)) & 1;
+	if (bit == 1)
+		kill(server_pid, SIGUSR1);
+	else
+		kill(server_pid, SIGUSR2);
 }
 
 void	send_string(int signum, siginfo_t *info, void *context)
@@ -43,27 +43,32 @@ void	send_string(int signum, siginfo_t *info, void *context)
 	(void)signum;
 	(void)info;
 	(void)context;
-	send_bit(g_send.pid, g_send.str[g_send.bit_index / 8]);
-	g_send.bit_index++;
+	if (g_send.bit_index <= (int)(ft_strlen(g_send.str) + 1) *8)
+	{
+		send_bit(g_send.pid, g_send.str[g_send.bit_index / 8]);
+		g_send.bit_index++;
+	}
+	else
+		return ;
 }
 
 int	main(int argc, char *argv[])
 {
-	struct sigaction sa;
-	int	i;
+	struct sigaction	sa;
+	int					i;
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_sigaction = send_string;
 	sa.sa_flags = SA_SIGINFO;
 	if (argc != 3)
 		return (1);
-	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
 		return (1);
 	i = 0;
 	g_send.pid = (pid_t)ft_atoi(argv[1]);
 	g_send.str = ft_strdup(argv[2]);
 	send_string(0, NULL, NULL);
-	while (g_send.bit_index < (int)(ft_strlen(g_send.str) + 1) * 8)
+	while (g_send.bit_index < (int)(ft_strlen(g_send.str) + 1) *8)
 		pause();
 	free(g_send.str);
 	return (0);
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: vstineau <vstineau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:40:17 by vstineau          #+#    #+#             */
-/*   Updated: 2024/02/27 15:32:52 by vstineau         ###   ########.fr       */
+/*   Updated: 2024/02/28 11:43:14 by vstineau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@
 typedef struct s_bit
 {
 	int		bit_received;
-	char	char_received;
+	char	char_get;
 	char	*str;
 	int		bit_index;
 	int		str_len;
 }					t_bit;
 
-static	t_bit	g_bit;
+static t_bit	g_bit;
 
 void	get_bit(int signal)
 {
@@ -34,22 +34,23 @@ void	get_bit(int signal)
 		g_bit.bit_received = 1;
 	else
 		g_bit.bit_received = 0;
-	g_bit.char_received = g_bit.char_received | (g_bit.bit_received << g_bit.bit_index);
+	g_bit.char_get = g_bit.char_get | (g_bit.bit_received << g_bit.bit_index);
 	g_bit.bit_index++;
 	if (g_bit.bit_index == 8)
 	{
 		g_bit.bit_index = 0;
 		g_bit.str = ft_realloc(g_bit.str, g_bit.str_len, (g_bit.str_len + 1));
-		g_bit.str[g_bit.str_len - 1] = g_bit.char_received;
-		g_bit.str_len = (int)ft_strlen(g_bit.str);
-		if (g_bit.char_received == 0)
+		g_bit.str[g_bit.str_len] = g_bit.char_get;
+		g_bit.str_len++;
+		if (g_bit.char_get == 0)
 		{
 			write(1, g_bit.str, g_bit.str_len);
+			write(1, "\n", 1);
 			free(g_bit.str);
 			g_bit.str = NULL;
-			g_bit.str_len = 1;
+			g_bit.str_len = 0;
 		}
-		g_bit.char_received = 0;
+		g_bit.char_get = 0;
 	}
 }
 
@@ -64,14 +65,14 @@ void	handler(int signum, siginfo_t *info, void *context)
 
 int	main(void)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_sigaction = handler;
 	sa.sa_flags = SA_SIGINFO;
-	g_bit.str_len = 1;
 	ft_printf("%d\n", getpid());
-	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+	if (sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
 		return (1);
 	while (1)
 		pause();
